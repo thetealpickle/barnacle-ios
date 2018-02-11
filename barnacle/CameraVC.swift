acc45cb5912d88322d42a6d23b2132cd6639628a
bald <jessjeanjo@gmail.com>
2018-02-10 19:10:25 -0500

    add camrea functionality, fix ui bugs

--- barnacle/CameraVC.swift
+++ barnacle/CameraVC.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraVC: UIViewController {
 
+    var captureSession: AVCaptureSession!
+    var cameraOutput: AVCapturePhotoOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
     @IBOutlet weak var capturedImageView: RoundedImageView!
     @IBOutlet weak var flashButton: RoundedShadowButton!
     @IBOutlet weak var identificationLabel: UILabel!
@@ -21,6 +26,42 @@ class CameraVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        previewLayer.frame = cameraView.bounds
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
+        
+        let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera!)
+            if captureSession.canAddInput(input) == true {
+                captureSession.addInput(input)
+            }
+            
+            cameraOutput = AVCapturePhotoOutput()
+            
+            if captureSession.canAddOutput(cameraOutput) == true {
+                captureSession.addOutput(cameraOutput)
+                
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+                previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
+                previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+                
+                cameraView.layer.addSublayer(previewLayer!)
+                captureSession.startRunning()
+            }
+        } catch {
+            debugPrint(error)
+        }
     }
 }
 
